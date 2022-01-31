feeed6cdc6e313ab87544ca23dabc6f2134476f1
IsmaelDev13 <ismael-diaz8@hotmail.es>
2022-01-31 11:49:14 +0100

    Private key Error

--- pages/api/submit-form.ts
+++ pages/api/submit-form.ts
@@ -10,7 +10,7 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
-    private_key: process.env.GOOGLE_PRIVATE_KEY!,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
   });
 
   await doc.loadInfo();
