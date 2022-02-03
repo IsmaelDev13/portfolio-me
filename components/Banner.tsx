8b3373018addeb0445b06800365ad8529709740d
IsmaelDev13 <ismael-diaz8@hotmail.es>
2022-02-03 14:10:00 +0100

    new image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -36,18 +36,12 @@ export const Banner = () => {
         <h1 className="stroke-4 relative mb-4 scale-y-90 transform select-none pl-8 pt-20 text-2xl font-bold uppercase leading-4 tracking-widest text-[#0F97B8] md:text-3xl">
           Who Am I?
         </h1>
-        <div className="group  items-center md:flex md:flex-row">
+        <div className="items-center md:flex md:flex-row">
           <div className="relative w-full">
             <img
               loading="lazy"
-              className=" relative my-10 mx-auto h-96 w-full rounded-full object-contain transition-transform duration-200 ease-in-out group-hover:scale-0 "
-              src="/sharpenpro.jpg"
-              alt=""
-            />
-            <img
-              loading="lazy"
-              className=" absolute top-0 my-10 mx-auto h-96  w-full scale-0 transform rounded-full object-contain transition-transform duration-150 ease-in  group-hover:scale-100"
-              src="/nobgfiread.png"
+              className=" relative my-10 mx-auto h-96 w-full rounded-full object-contain"
+              src="/nobgsh.jpg"
               alt=""
             />
           </div>
